56bb6ed989badef4d39eb3f42012823cc490f210
Denis G <denis1610@yahoo.com>
2021-09-16 23:38:52 +0300

    Finished functionality, starting GUI
    Finished all the required functionality, now the application retrieves the author ID using the name through Scholarly package, later on this ID is passed to search all author's publication through SerpAPI, the output results are extracted and can later be sorted accordingly.

--- authors.py
+++ authors.py
@@ -1,5 +1,6 @@
 from scholarly import scholarly
 from serpapi import GoogleSearch
+import PySimpleGUI as sg
 import pprint
 
 author_name = 'Arnold Meijster'
@@ -16,13 +17,22 @@ serp_params = {
   "hl": "en",
   "author_id": author_id,
   "num": "10",  #Number of return results after the search (SerpAPI max = 100)
-  "start": "100",  #Result offset (0 = first page of results, 20 = second page, 40 = third and so on)
+  "start": "0",  #Result offset (0 = first page of results, 20 = second page, 40 = third and so on)
   "api_key": "8007fc0df66260445ed4fda679c1ae63511e60bb0b13e6baa9b4b5a46acffe0b"  # Here input your OWN SerpAPI key
 }
 
+# Searching with the above parameters for the results through SerpAPI and saving results in a dictionary
 search = GoogleSearch(serp_params)
 authors_dict = search.get_dict()
 
-# for x in authors_dict :
+# Converting the values of the output's dictionary into a list to make further operations with it
+x = list(authors_dict.values())
+final_data = []
 
-pprint.pprint(authors_dict)
+# Loop to traverse the list and extract all the articles with their respective title, authors, year and citation and save them into a separate list of tuples.
+for i in range(0, len(x[0])) :
+  data_tuple = (x[0][i]['title'], x[0][i]['authors'], x[0][i]['year'], x[0][i]['cited_by']['value'])
+  final_data.append(data_tuple)
+
+# User will select whether he would like to sort the information by year or by citations number. In case of year - index = 2, in case of citations - index = 3.
+pprint.pprint(sorted(final_data, key=lambda year: year[3]))
